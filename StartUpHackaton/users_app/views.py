e0928f4de9f4b8673619948d2f1bb047c887251a
behem0thik <behem0thik@users.noreply.github.com>
2022-09-15 16:25:53 +0600

    flash messages

--- StartUpHackaton/users_app/views.py
+++ StartUpHackaton/users_app/views.py
@@ -1,9 +1,10 @@
 import logging
 
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +15,14 @@ def index(request):
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
     context = {'form': form}
     template_name = 'users/register.html'
     return render(request, template_name, context)
